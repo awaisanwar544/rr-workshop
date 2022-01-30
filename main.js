c75ba5fa10aaea15dd24f390e6e8e5dc4155a972
awaisanwar544 <awaisanwar544@gmail.com>
2022-01-31 00:25:01 +0500

    Add mobile menu icon

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const topHeight = () => {
-  const height = document.querySelector('header').offsetHeight;
-  document.getElementById('hero').style.height = `calc(100vh - ${height}px)`;
+const divSize = (element, height) => {
+  document.getElementById(element).style.height = `calc(100vh - ${height}px)`;
 };
 
-topHeight();
+const headerHeight = document.querySelector('header').offsetHeight;
+divSize('hero', headerHeight);
+divSize('program', headerHeight * 2);
+
+const menuBtn = document.querySelector('#menuBtn');
+let open = false;
+menuBtn.addEventListener('click', () => {
+  const menuIcon = document.querySelector('#menuIcon');
+  if (open) {
+    menuIcon.className = 'fas fa-bars';
+    open = false;
+  } else {
+    menuIcon.className = 'fas fa-times';
+    open = true;
+  }
+});
